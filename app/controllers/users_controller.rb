035b79622b49e61ba732639ae2bceaf88cd055cb
acupoftee <teedia31@gmail.com>
2019-07-23 14:32:45 -0400

    Fix Read Issue for Users
    Fixed index and show methods in users_controller.rb.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class UsersController < ProtectedController
+class UsersController < OpenReadController
   skip_before_action :authenticate, only: %i[signup signin]
 
   # POST '/sign-up'
@@ -46,6 +46,43 @@ class UsersController < ProtectedController
     end
   end
 
+  # GET /user
+  def index
+    @user = User.all
+
+    render json: @user
+  end
+
+  # GET /user/1
+  def show
+    render json: @user
+  end
+
+  # # POST /user
+  # def create
+  #   @user = User.new(user_params)
+  #
+  #   if @user.save
+  #     render json: @user, status: :created, location: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
   private
 
   def user_creds
@@ -56,5 +93,16 @@ class UsersController < ProtectedController
   def pw_creds
     params.require(:passwords)
           .permit(:old, :new)
+  end
+
+  # def set_users
+  #   @user = User.find(params[:id])
+  # end
+
+  def user_params
+    params.require(:user).permit(:id, :email, :display_name,
+                                 :summary, :is_mentor, :platform,
+                                 :region, :avatar_url, :skill_rating,
+                                 :video_id, :hero_id)
   end
 end
