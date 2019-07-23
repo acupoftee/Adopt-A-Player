dccac9561313b107fbff08d4e921960d270c7905
acupoftee <teedia31@gmail.com>
2019-07-23 12:04:24 -0400

    Add Profile Attributes to User Serializer
    Adding profile serializers to user_serializer.rb.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  attributes :id, :email, :display_name,
+             :summary, :is_mentor, :platform,
+             :region, :avatar_url, :skill_rating
   has_many :videos
   has_many :heros
 end
